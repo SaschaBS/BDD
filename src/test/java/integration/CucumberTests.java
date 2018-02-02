acfa3de172ef2b3032380684f37235ca7ea2dc92
Sascha.Hoppe <sascha.hoppe@lineas.de>
2018-02-02 16:22:18 +0100

    behave pro task implemented

--- src/test/java/integration/CucumberTests.java
+++ src/test/java/integration/CucumberTests.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         strict = true,
-//        features = "src/test/resources/features",
-        features = {"build/resources/test/features", "build/generated-test-sources/cucumber"},
+        features = "src/test/resources/features",
+//        features = {"build/resources/test/features", "build/generated-test-sources/cucumber"},
         format = {
                 "json:build/reports/cucumber/cucumber-report.json",
                 "html:build/reports/cucumber/plain-html-reports"
