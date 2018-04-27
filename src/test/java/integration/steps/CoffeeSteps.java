c18f8a2f1902954abae0001c724861e0cc6714c3
sho <sascha.hoppe@lineas.de>
2018-04-27 15:17:44 +0200

    Minimal Test set

--- src/test/java/integration/steps/CoffeeSteps.java
+++ src/test/java/integration/steps/CoffeeSteps.java
@@ -2,11 +2,15 @@ package integration.steps;
 
 import com.codeborne.selenide.Configuration;
 import cucumber.api.java.Before;
+import cucumber.api.java.de.Angenommen;
+import cucumber.api.java.de.Und;
 import integration.pages.CoffeeMachinePage;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.Assert.assertFalse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = {de.lineas.bddemo.Application.class})
@@ -22,4 +26,16 @@ public class CoffeeSteps {
         Configuration.baseUrl = "http://localhost:8082";
         page = new CoffeeMachinePage();
     }
+
+    @Angenommen("^Ich betrachte die Kaffeemaschine$")
+    public void ichBetrachteDieKaffeemaschine() throws Throwable {
+        page.open();
+    }
+
+    @Und("^die Kaffemaschine wurde noch nicht eingeschaltet$")
+    public void dieKaffemaschineWurdeNochNichtEingeschaltet() throws Throwable {
+        assertFalse(page.isOn());
+    }
+
+
 }
