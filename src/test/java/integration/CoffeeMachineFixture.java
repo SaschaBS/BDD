92b1ca0ac8c50635b18428e8e3ddc4ead2861063
sho <sascha.hoppe@lineas.de>
2018-05-14 09:52:41 +0200

    enable only fixture

--- src/test/java/integration/CoffeeMachineFixture.java
+++ src/test/java/integration/CoffeeMachineFixture.java
@@ -1,66 +1,66 @@
-//package integration;
-//
-//import com.codeborne.selenide.Configuration;
-//import integration.pages.CoffeeMachinePage;
-//import io.github.bonigarcia.wdm.ChromeDriverManager;
-//import org.chiknrice.test.SpringifiedConcordionRunner;
-//import org.concordion.api.ConcordionResources;
-//import org.junit.Before;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ContextConfiguration;
-//
-//@SuppressWarnings("TestFixtureIsNotAnnotatedWithConcordionRunner")
-//@RunWith(SpringifiedConcordionRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@ContextConfiguration(classes = {de.lineas.bddemo.Application.class})
-//@ConcordionResources({"images/machine.jpg"})
-//public class CoffeeMachineFixture {
-//
-//    private CoffeeMachinePage page;
-//
-//    @Before
-//    public void setup() {
-//
-//        ChromeDriverManager.getInstance().setup();
-//        Configuration.browser = "chrome";
-//        Configuration.baseUrl = "http://localhost:8082";
-//        page = new CoffeeMachinePage();
-//        page.open();
-//        page.setLanguage("de");
-//    }
-//
-//    public boolean isNotTurnedOn() {
-//        return !page.isOn();
-//    }
-//
-//    public String getMessage() {
-//        return page.getMessage();
-//    }
-//
-//    public void turnOn() {
-//        page.turnOn();
-//    }
-//
-//    public boolean isTurnedOn() {
-//        return page.isOn();
-//    }
-//
-//    public void turnOnAndBrew() {
-//        page.turnOn();
-//        page.brew();
-//    }
-//
-//    public boolean hasPot() {
-//        return page.hasPot();
-//    }
-//
-//    public boolean hasCoffee() {
-//        return page.hasCoffee();
-//    }
-//
-//    public boolean hasReminderMessage() {
-//        return getMessage().equals("Please insert coffee pot!");
-//    }
-//
-//}
+package integration;
+
+import com.codeborne.selenide.Configuration;
+import integration.pages.CoffeeMachinePage;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.chiknrice.test.SpringifiedConcordionRunner;
+import org.concordion.api.ConcordionResources;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SuppressWarnings("TestFixtureIsNotAnnotatedWithConcordionRunner")
+@RunWith(SpringifiedConcordionRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = {de.lineas.bddemo.Application.class})
+@ConcordionResources({"images/machine.jpg"})
+public class CoffeeMachineFixture {
+
+    private CoffeeMachinePage page;
+
+    @Before
+    public void setup() {
+
+        ChromeDriverManager.getInstance().setup();
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "http://localhost:8082";
+        page = new CoffeeMachinePage();
+        page.open();
+        page.setLanguage("de");
+    }
+
+    public boolean isNotTurnedOn() {
+        return !page.isOn();
+    }
+
+    public String getMessage() {
+        return page.getMessage();
+    }
+
+    public void turnOn() {
+        page.turnOn();
+    }
+
+    public boolean isTurnedOn() {
+        return page.isOn();
+    }
+
+    public void turnOnAndBrew() {
+        page.turnOn();
+        page.brew();
+    }
+
+    public boolean hasPot() {
+        return page.hasPot();
+    }
+
+    public boolean hasCoffee() {
+        return page.hasCoffee();
+    }
+
+    public boolean hasReminderMessage() {
+        return getMessage().equals("Please insert coffee pot!");
+    }
+
+}
--- src/test/java/integration/CucumberTests.java
+++ src/test/java/integration/CucumberTests.java
@@ -1,18 +1,18 @@
-package integration;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        strict = true,
-        features = "src/test/resources/features",
-//        features = {"build/resources/test/features", "build/generated-test-sources/cucumber"},
-        format = {
-                "json:build/reports/cucumber/cucumber-report.json",
-                "html:build/reports/cucumber/plain-html-reports"
-        })
-public class CucumberTests {
-
-}
+//package integration;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        strict = true,
+//        features = "src/test/resources/features",
+////        features = {"build/resources/test/features", "build/generated-test-sources/cucumber"},
+//        format = {
+//                "json:build/reports/cucumber/cucumber-report.json",
+//                "html:build/reports/cucumber/plain-html-reports"
+//        })
+//public class CucumberTests {
+//
+//}
